a7b9973584d711c97ef6d734cb1bc85ce58f4d6e
Michal Trojanowski <mtroja98@gmail.com>
2024-05-26 08:16:57 +0200

    finalized

--- src/Present.java
+++ src/Present.java
@@ -1,11 +1,16 @@
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Present {
-
-	public Present() {};
+	
+	int totalWeight = 0;
+	int lastArrayIndex = 0;
+	
 	
 	Sweet[] sweets = new Sweet[10];
-	Sweet[] newSweets = new Sweet[10];
+	Sweet[] presentSweets = new Sweet[10];
+
+	public Present() {};
 	
 	// the method filters sweets by sugar weight inclusively 
 	public Sweet[] filterSweetsBySugarRange(double minSugarWeight, double maxSugarWeight) {
@@ -17,21 +22,26 @@ public class Present {
 
 	// the method calculates total weight of the present
 	public double calculateTotalWeight() {
-		return 0;
+	
+		return Arrays.stream(sweets)
+				.filter(Objects::nonNull)
+				.mapToDouble(sweet -> sweet.getWeight())
+				.sum();
 	}
 
 	// the method that adds sweet to the present
 	public void addSweet(Sweet sweet) {
 		
 	}
+	
+
 
 	// the method returns copy of the Sweet[] array so that nobody could 
 	// modify state of the present without addSweet() method. 
 	// Also array shouldn’t contain null values.
 	public Sweet[] getSweets() {
 		
-		
-		return null;
+		return Arrays.stream(sweets).filter(Objects::nonNull).toArray(Sweet[]::new);
 	}
 
 	
--- src/Sweet.java
+++ src/Sweet.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public abstract class Sweet {
 	String name;
@@ -10,8 +11,53 @@ public abstract class Sweet {
 		this.weight = weight;
 		this.sugarWeight = sugarWeight;
 	}
-	
-	public double getSugarWeight () {
-		return this.sugarWeight;
+
+	public double getSugarWeight() {
+		return sugarWeight;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public void setSugarWeight(double sugarWeight) {
+		this.sugarWeight = sugarWeight;
+	}
+
+	@Override
+	public String toString() {
+		return "Sweet [name=" + name + ", weight=" + weight + ", sugarWeight=" + sugarWeight + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, sugarWeight, weight);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sweet other = (Sweet) obj;
+		return Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(sugarWeight) == Double.doubleToLongBits(other.sugarWeight)
+				&& Double.doubleToLongBits(weight) == Double.doubleToLongBits(other.weight);
+	}
+	
 }
